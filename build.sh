40293a75f1bb241c0b4fbea290586b784ba6018a
sky line <32512284+linesky@users.noreply.github.com>
2024-03-27 20:46:21 +0000

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,4 +1,16 @@
 nasm hello.asm -o a.exe
 fasm cmd.asm load.com
+fasm beep.asm beep.exe
+fasm sound.asm sound.exe
+fasm help.asm help.exe
+fasm exit.asm exit.exe
+fasm cls.asm cls.exe
+fasm sleep.asm sleep.exe
+fasm time.asm time.exe
+fasm date.asm date.exe
+fasm clock.asm clock.exe
+fasm shutdown.asm shutdown.exe
+fasm line.asm line.exe
+fasm echo.asm echo.exe
 file a.exe
 dosbox ./
